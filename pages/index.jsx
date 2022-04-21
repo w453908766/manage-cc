d16880c44a6a877e32435291782036aec5d7d771
kukuchick <w453908766@126.com>
2022-04-21 20:17:27 +0800

    add title

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {Audit} from '../components/audit'
+import { Audit } from '../components/audit'
 import config from '../config.json'
 import { useState, useEffect, useCallback, useMemo } from 'react'
 
 
 export default function Main() {
   return (
-    <Audit />
+    <div>
+      <Head>
+        <title>Manage CC</title>
+      </Head>
+      <Audit />
+    </div>
   )
 }
